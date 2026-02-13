ea0fe523f380bcf74fd3d1255d0c53de431d60b0
arshdeep71 <darkwebhost01@gmail.com>
2026-02-13 11:34:54 +0530

    updated

---
+++ file27.js
@@ -0,0 +1,60 @@
+// let score = 90;
+// let result
+// if(score>50){
+//     result = "pass";
+// }
+// else{
+//     result = "Fail";
+// }
+// console.log(result);
+
+
+
+// let score = 40;
+// let result = score > 50 ? "Pass" : "Fail";
+// let result = score > 50 ? "Pass" : "Fail";
+// let result = score > 50 && "pass";
+// console.log(result);
+
+
+// let score
+// let result = score || 30
+// console.log(result);
+
+
+let score = 50;
+
+switch (true) {
+    case score >= 80:
+        console.log("Grade A");
+        break;
+
+    case score >= 70:
+        console.log("Grade B");
+        break;
+
+    case score >= 60:
+        console.log("Grade C");
+        break;
+
+    default:
+        console.log("Fail");
+}
+
+
+let subject = "HTML";
+switch(subject){
+    case "HTML":{
+        console.log("HyperText Markup Language");
+        break;
+    }
+
+     case "CSS":{
+        console.log("Cascading Style Sheet");
+        break;
+    }
+     case "HTML":{
+        console.log("HyperText Markup Language");
+        break;
+    }
+}
---
+++ file28.js
@@ -0,0 +1,4 @@
+const names = ["Jonh","Cathy","Amy"]
+names.forEach((name,index,a) => {
+    console.log(index)
+});
