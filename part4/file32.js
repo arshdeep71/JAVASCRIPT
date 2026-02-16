0bcf895de97d087c12025f2623c9c28d2295b7fe
arshdeep71 <darkwebhost01@gmail.com>
2026-02-16 10:01:45 +0530

    updated

--- part4/file32.js
+++ part4/file32.js
@@ -6,7 +6,30 @@ const numbers = [10,20,30,40,50]
 
 
 // DESTRUCTURING NEW METHOD OF ES6 TO EXTRACT ARRAY ELEMENTS
-const [n1,n2,...n3] = numbers
-console.log(n1);
-console.log(n2);
-console.log(n3);
+// const [n1,n2,...n3] = numbers
+// console.log(n1);
+// console.log(n2);
+// console.log(n3);
+
+
+
+// ...rest operator example
+// const [n1,n2,...n3] = numbers
+// console.log(n1);
+// console.log(n2);
+// console.log(n3);
+
+
+
+//skip values example by comma
+// const [n1,,n2] = numbers;
+// console.log(n1)
+// console.log(n2)
+
+
+
+//default value example
+const [n1,n2,n3=10] = [12,15,20]
+console.log(n1)
+console.log(n2)
+console.log(n3)
