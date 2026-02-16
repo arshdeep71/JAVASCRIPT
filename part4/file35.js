5ac078c3d07c93c4617581782ee12dcaf86b53d2
arshdeep71 <darkwebhost01@gmail.com>
2026-02-16 11:15:56 +0530

    updated

--- part4/file35.js
+++ part4/file35.js
@@ -1,9 +1,22 @@
-const names = ["Arsh","Varinder","Harsh","Pari","leen"]
+// const names = ["Arsh","Varinder","Harsh","Pari","leen"]
+const names = new Array("Arsh","Varinder","Harsh","Pari","leen")
+
 // names.push("Vivek");
 // names.pop()
 
 //SHIFT and UNSHIFT is slower because of rearrangement
 // names.shift()
 // names.unshift("Varinder")
+// console.log(names)
 
-console.log(names)
+// console.log(names.sort())
+// console.log(names.reverse())
+
+const number = [5,2,4,6,1,2];
+// console.log(number.sort((a,b)=>a-b));
+// console.log(number.sort((a,b)=>b-a));
+
+// console.log(number.indexOf(2));
+// console.log(number.lastIndexOf(2));
+
+console.log(number.includes(3));
