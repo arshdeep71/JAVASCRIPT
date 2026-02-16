cb588867bbcd454db5e98960ace0747ac5729fed
arshdeep71 <darkwebhost01@gmail.com>
2026-02-16 09:30:17 +0530

    updated

---
+++ file29.js
@@ -0,0 +1,7 @@
+function add() {
+    console.log(arguments)
+    console.log(arguments.length);
+    console.log(arguments[0])
+}
+
+add(7,5,8,9)
---
+++ part4/file31.js
@@ -0,0 +1,9 @@
+import Footer from "./footer.js";
+import Header from "./header.js";
+import Products from "./products.js";
+
+console.log(Header())
+console.log(".........................")
+console.table(Products())
+console.log(".........................")
+console.log(Footer())
