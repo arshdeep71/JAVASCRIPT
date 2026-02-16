3ad1ce113de6ad7c3378e48ce89119df7e665234
arshdeep71 <darkwebhost01@gmail.com>
2026-02-16 10:57:24 +0530

    updated

---
+++ part4/file35.js
@@ -0,0 +1,9 @@
+const names = ["Arsh","Varinder","Harsh","Pari","leen"]
+// names.push("Vivek");
+// names.pop()
+
+//SHIFT and UNSHIFT is slower because of rearrangement
+// names.shift()
+// names.unshift("Varinder")
+
+console.log(names)
