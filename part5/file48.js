e101aae9b45f65ae460808d981089520443fb79e
arshdeep71 <darkwebhost01@gmail.com>
2026-02-17 11:27:09 +0530

    updated

--- part5/file48.js
+++ part5/file48.js
@@ -1,7 +1,7 @@
 const students = [
-    { id: 1, name: "Arsh", s1: true , s2: true, s3: false},
+    { id: 1, name: "Vansh", s1: true , s2: true, s3: false},
     { id: 2, name: "Ravleen",s1: true , s2: true, s3: false},
     { id: 3, name: "Varinder",s1: true , s2: true, s3: false},
 ];
 
-// if id is 1 > Arsh is absent
+// if id is 1 > Vansh is absent
